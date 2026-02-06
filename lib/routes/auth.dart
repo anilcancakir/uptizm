3e94e4fb98111017fec391913a9508f587274901
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-06 19:24:06 +0300

    feat(status-page): register app routes and update navigation

--- lib/routes/auth.dart
+++ lib/routes/auth.dart
@@ -12,16 +12,22 @@ void registerAuthRoutes() {
     middleware: ['guest'],
     layout: (child) => GuestLayout(child: child),
     routes: () {
-      MagicRoute.page('/login', AuthController.instance.login)
-          .transition(RouteTransition.none);
-      MagicRoute.page('/register', AuthController.instance.register)
-          .transition(RouteTransition.none);
+      MagicRoute.page(
+        '/login',
+        AuthController.instance.login,
+      ).transition(RouteTransition.none);
+      MagicRoute.page(
+        '/register',
+        AuthController.instance.register,
+      ).transition(RouteTransition.none);
       MagicRoute.page(
         '/forgot-password',
         AuthController.instance.forgotPassword,
       ).transition(RouteTransition.none);
-      MagicRoute.page('/reset-password', AuthController.instance.resetPassword)
-          .transition(RouteTransition.none);
+      MagicRoute.page(
+        '/reset-password',
+        AuthController.instance.resetPassword,
+      ).transition(RouteTransition.none);
     },
   );
 }
